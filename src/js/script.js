8f7dd08e60a20840ae7bac3aa57fe5b2d25d4632
Marat <yeffasol@mail.ru>
2019-09-08 20:51:20 +0300

    Get data with fetch

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,13 @@
 function toggleCheckbox() {
 	const checkboxes = document.querySelectorAll(".filter-check_checkbox");
 
-	Array.prototype.forEach.call(checkboxes, function (element) {
-		element.addEventListener("change", function () {
+	checkboxes.forEach(element => {
+		element.addEventListener("change", () => {
 			if (element.checked) {
 				element.nextElementSibling.classList.add("checked");
 			} else {
 				element.nextElementSibling.classList.remove("checked");
 			}
-
 		});
 	});
 }
@@ -20,16 +19,16 @@ function toggleCart() {
 	const modalCart = document.querySelector(".cart");
 	const closeBtn = document.querySelector(".cart-close");
 
-	btnCart.addEventListener("click", function () {
+	btnCart.addEventListener("click", () => {
 		modalCart.style.display = "flex";
 		document.body.style.overflow = "hidden";
-	})
+	});
 
-	closeBtn.addEventListener("click", function () {
+	closeBtn.addEventListener("click", () => {
 		modalCart.style.display = "";
 		document.body.style.overflow = "";
 
-	})
+	});
 }
 
 function toggleCard() {
@@ -38,21 +37,21 @@ function toggleCard() {
 	const cartEmpty = document.getElementById("cart-empty");
 	const cartCount = document.querySelector(".counter");
 
-	Array.prototype.forEach.call(cards, function (card) {
+	cards.forEach( card => {
 		const btn = card.querySelector("button");
 
-		btn.addEventListener("click", function () {
+		btn.addEventListener("click", () => {
 			const cardClone = card.cloneNode(true);
 			cartWrapper.appendChild(cardClone);
 			showData();
 			const removeBtn = cardClone.querySelector(".btn");
 			removeBtn.textContent = "Удалить из корзины";
 
-			removeBtn.addEventListener("click", function () {
+			removeBtn.addEventListener("click", () => {
 				cardClone.remove();
 				showData();
-			})
-		})
+			});
+		});
 	});
 
 	function showData() {
@@ -63,7 +62,7 @@ function toggleCard() {
 		let sum = 0;
 
 		cartCount.textContent = cardsCart.length;
-		Array.prototype.forEach.call(cardPrices, function (el) {
+		cardPrices.forEach( el => {
 			const price = parseFloat(el.textContent);
 
 			sum += price;
@@ -86,7 +85,7 @@ function actionPage() {
 	const searchBtn = document.querySelector(".search-btn");
 
 	function filter() {
-		Array.prototype.forEach.call(cards, function (card) {
+		cards.forEach(card => {
 			const cardPrice = card.querySelector(".card-price");
 			const price = parseFloat(cardPrice.textContent);
 			const discount = card.querySelector(".card-sale");
@@ -100,13 +99,13 @@ function actionPage() {
 		});
 	}
 
-	discountCheckbox.addEventListener("change", filter)
+	discountCheckbox.addEventListener("change", filter);
 	min.addEventListener("change", filter);
 	max.addEventListener("change", filter);
 	searchBtn.addEventListener("click", function () {
 		const searchText = new RegExp(search.value.trim(), "i");
 
-		Array.prototype.forEach.call(cards, function (card) {
+		cards.forEach(card => {
 			const title = card.querySelector(".card-title").textContent;
 
 			card.parentNode.style.display = "";
@@ -114,10 +113,52 @@ function actionPage() {
 				card.parentNode.style.display = "none";
 			}
 		});
-	})
+	});
 }
 
-toggleCheckbox()
-toggleCart()
-toggleCard()
-actionPage()
+function getData() {
+	const goods = document.querySelector(".goods");
+	return fetch("../../db/db.json")
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw new Error(`Данные не были получены. Ошибка: ${response.status}`);
+			}
+		})
+		.then(data => {
+			return data;
+		})
+		.catch(err => goods.textContent = err);
+}
+
+function renderCards(data) {
+	const goods = document.querySelector(".goods");
+	data.goods.forEach(item => {
+		const card = document.createElement("div");
+		card.className = "col-12 col-md-6 col-lg-4 col-xl-3";
+		card.innerHTML = `<div class="card">
+								${item.sale ? '<div class="card-sale">🔥Hot Sale🔥</div>' : ''} 
+                                <div class="card-img-wrapper">
+										<span class="card-img-top"
+                                              style="background-image: url(${item.img})"></span>
+                                </div>
+                                <div class="card-body justify-content-between">
+                                    <div class="card-price">${item.price} ₽</div>
+                                    <h5 class="card-title">${item.title}</h5>
+                                    <button class="btn btn-primary">В корзину</button>
+                                </div>
+                            </div>
+`;
+		goods.appendChild(card);
+	});
+}
+
+getData().then(data => {
+	renderCards(data);
+	toggleCheckbox();
+	toggleCart();
+	toggleCard();
+	actionPage();
+});
+
